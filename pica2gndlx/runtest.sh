02176d90edd800842a9310b39b40a121bbd2f6b9
Jakob Voss <voss@gbv.de>
2018-08-16 12:19:42 +0200

    Remove GNDLX

--- pica2gndlx/runtest.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../testrunner pica2gndlx.xsl $*
--- pica2gndlx/tags.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Get a list of tags used in GNDLightXML (GNDXL)
-xsltproc tags.xsl pica2gndlx.xsl | grep -v Error | LC_ALL=C sort | uniq
